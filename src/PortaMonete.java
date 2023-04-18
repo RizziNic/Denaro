eb3331b95e8bc2cd15a47f0c0d0f581dff2a7f07
RizziNic <rizzijunior.nico@gmail.com>
2023-04-18 10:18:38 +0200

    1

---
+++ src/PortaMonete.java
@@ -0,0 +1,11 @@
+public class PortaMonete {
+
+    double monetacinquanta;
+    int moneta1;
+    int moneta2;
+    void PortaMonete(double monetacinquanta, int moneta1, int moneta2){
+        this.monetacinquanta = 0;
+        this.moneta1 = 0;
+        this.moneta2 = 0;
+    }
+}
